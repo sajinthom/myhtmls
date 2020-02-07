1e8f9f58cf058fb111218bfa9ca527bd3e25777e
sajinthom <49722694+sajinthom@users.noreply.github.com>
2020-02-07 16:10:16 +1100

    Made it a plugin

--- BackForwardButtons.js
+++ BackForwardButtons.js
@@ -1,34 +1,27 @@
-videojs.getPlayer("myPlayerID").ready(function () {
-    // +++ Create divs for buttons +++
+videojs.registerPlugin('backForwardButtons', function () {
     var myPlayer = this,
         jumpAmount = 5,
         controlBar,
         insertBeforeNode,
-        newElementBB = document.createElement("div"),
-        newElementFB = document.createElement("div"),
-        newImageBB = document.createElement("img"),
-        newImageFB = document.createElement("img");
+        newElementBB = document.createElement('div'),
+        newElementFB = document.createElement('div'),
+        newImageBB = document.createElement('img'),
+        newImageFB = document.createElement('img');
 
     // +++ Assign IDs for later element manipulation +++
-    newElementBB.id = "backButton";
-    newElementFB.id = "forwardButton";
+    newElementBB.id = 'backButton';
+    newElementFB.id = 'forwardButton';
 
     // +++ Assign properties to elements and assign to parents +++
-    newImageBB.setAttribute(
-        "src",
-        "//learning-services-media.brightcove.com/doc-assets/player-development/samples/back-forward-buttons/back-button.png"
-    );
+    newImageBB.setAttribute('src', 'https://learning-services-media.brightcove.com/doc-assets/player-development/samples/back-forward-buttons/back-button.png');
     newElementBB.appendChild(newImageBB);
-    newImageFB.setAttribute(
-        "src",
-        "//learning-services-media.brightcove.com/doc-assets/player-development/samples/back-forward-buttons/forward-button.png"
-    );
+    newImageFB.setAttribute('src', 'https://learning-services-media.brightcove.com/doc-assets/player-development/samples/back-forward-buttons/forward-button.png');
     newElementFB.appendChild(newImageFB);
 
     // +++ Get controlbar and insert elements +++
-    controlBar = myPlayer.$(".vjs-control-bar");
+    controlBar = myPlayer.$('.vjs-control-bar');
     // Get the element to insert buttons in front of in conrolbar
-    insertBeforeNode = myPlayer.$(".vjs-volume-panel");
+    insertBeforeNode = myPlayer.$('.vjs-volume-panel');
 
     // Insert the button div in proper location
     controlBar.insertBefore(newElementBB, insertBeforeNode);
@@ -36,7 +29,7 @@ videojs.getPlayer("myPlayerID").ready(function () {
 
     // +++ Add event handlers to jump back or forward +++
     // Back button logic, don't jump to negative times
-    newElementBB.addEventListener("click", function () {
+    newElementBB.addEventListener('click', function () {
         var newTime,
             rewindAmt = jumpAmount,
             videoTime = myPlayer.currentTime();
@@ -49,7 +42,7 @@ videojs.getPlayer("myPlayerID").ready(function () {
     });
 
     // Forward button logic, don't jump past the duration
-    newElementFB.addEventListener("click", function () {
+    newElementFB.addEventListener('click', function () {
         var newTime,
             forwardAmt = jumpAmount,
             videoTime = myPlayer.currentTime(),
