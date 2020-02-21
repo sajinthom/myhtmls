f09421cec7c4edb2dd165144fadd785a082a49b3
sajinthom <49722694+sajinthom@users.noreply.github.com>
2020-02-21 15:28:18 +1100

    Update plugin-telstra-sign.js

--- plugin-telstra-sign.js
+++ plugin-telstra-sign.js
@@ -1,14 +1,14 @@
 var registerPlugin = videojs.registerPlugin || videojs.plugin;
+
 var defaults = {};
+
 var onPlayerReady = function (player, options) {
     videojs.use('*', function (player) {
         return {
             setSource(srcObj, next) {
                 // modify source URL here
                 // source url will be available in srcObj.src
-                var src = sign(srcObj.src(), options);
-                console.log(src);
-                srcObj.src = src;
+                srcObj.src = sign(srcObj.src(), options);
                 next(null, srcObj);
             }
         };
