a73c593484ae1f6fae4a13e8e352e04725cc0d3f
sajinthom <49722694+sajinthom@users.noreply.github.com>
2020-07-31 14:32:37 +1000

    Create plugin-akamai-analytics.js

---
+++ plugin-akamai-analytics.js
@@ -0,0 +1,8 @@
+videojs.registerPlugin('akamaiAnalytics', function () {
+    var myPlayer = this;
+    var analyticsLoader = new Brightcove_AkamaiMediaAnalytics("https://ma1487-r.analytics.edgekey.net/config/beacon-28762.xml");
+  analyticsLoader.setMediaPlayer(videojs('samplePlayer'));
+        }
+    });
+
+});
