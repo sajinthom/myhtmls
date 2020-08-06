ad80db059122af6707ee37df2401a028999e2fe5
sajinthom <49722694+sajinthom@users.noreply.github.com>
2020-08-06 16:07:27 +1000

    Fixing syntax errors

--- plugin-akamai-analytics.js
+++ plugin-akamai-analytics.js
@@ -2,7 +2,4 @@ videojs.registerPlugin('akamaiAnalytics', function () {
     var myPlayer = this;
     var analyticsLoader = new Brightcove_AkamaiMediaAnalytics("https://ma1487-r.analytics.edgekey.net/config/beacon-28762.xml");
   analyticsLoader.setMediaPlayer(myPlayer);
-        }
-    });
-
 });
