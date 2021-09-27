40d56d23ad2c95bd327124d4c77aff7a9d3bf566
sajinthom <49722694+sajinthom@users.noreply.github.com>
2021-09-27 17:24:27 +1000

    Update plugin-bc-hideInbandCCLabel.js

--- plugin-bc-hideInbandCCLabel.js
+++ plugin-bc-hideInbandCCLabel.js
@@ -1,9 +1,16 @@
-videojs.registerPlugin('hideInbandCCLabel', function() {
-    if (videojs.browser.IS_SAFARI) {
-        return;
-    }
-    var player = this;
-    player.on('loadedmetadata', function() {
-        player.controlBar.subsCapsButton.items[3].el().style.display = 'none'
-    });
-});
+videojs.registerPlugin('hideInbandCCLabel', function () { 
+if (videojs.browser.IS_SAFARI) { 
+return; 
+} 
+var player = this; 
+subCapButton = player.controlBar.subsCapsButton; 
+subCapButton.one(['click', 'mouseover'], function () { 
+var items = subCapButton.items; 
+
+for (var i = 0; i < items.length; i++) { 
+if (items[i].track.kind === 'captions') { 
+items[i].el().style.display = 'none'; 
+} 
+} 
+}); 
+}); 
