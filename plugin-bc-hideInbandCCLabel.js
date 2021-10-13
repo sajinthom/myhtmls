07ea8644d7289ebc6e972ef50042d34fb4f1e68a
sajinthom <49722694+sajinthom@users.noreply.github.com>
2021-10-13 15:37:17 +1100

    Update plugin-bc-hideInbandCCLabel.js

--- plugin-bc-hideInbandCCLabel.js
+++ plugin-bc-hideInbandCCLabel.js
@@ -1,20 +1,22 @@
 videojs.registerPlugin('hideInbandCCLabel', function () {
-    var player = this;
-    subCapButton = player.controlBar.subsCapsButton;
-    subCapButton.one(['click', 'mouseover'], function () {
-        var items = subCapButton.items;
-        for (var i = 0; i < items.length; i++) {
+        var player = this;
+        subCapButton = player.controlBar.subsCapsButton;
+        subCapButton.one(['click', 'mouseover'], function () {
+          var items = subCapButton.items;
+          for (var i = 0; i < items.length; i++) {
             if (items[i].track.kind === 'captions') {
-                items[i].el().style.display = 'none';
+              items[i].el().style.display = 'none';
             }
-        }
-    });
-    player.one('playing', function () {
+          }
+        });
+
         var tt = player.textTracks();
-        for (var i = 0; i < tt.length; i++) {
+        tt.addEventListener('change', function (e) {
+          for (var i = 0; i < tt.length; i++) {
             if (tt[i].mode === 'showing') {
-                tt[i].mode = 'disabled';
+              tt[i].mode = 'hidden';
+              this.removeEventListener('change', arguments.callee);
             }
-        }
-    });
-});
+          }
+        });
+      });
