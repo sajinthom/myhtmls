8cc0e425915301d9584982ec6bc222872ccd9b8e
sajinthom <49722694+sajinthom@users.noreply.github.com>
2022-02-16 17:22:57 +1100

    Create plugin-bc-hideCCLabel-test.js

---
+++ plugin-bc-hideCCLabel-test.js
@@ -0,0 +1,22 @@
+videojs.registerPlugin('hideInbandCCLabel', function () {
+    var player = this;
+    subCapButton = player.controlBar.subsCapsButton;
+    subCapButton.one(['click', 'mouseover'], function () {
+        var items = subCapButton.items;
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].track.kind === 'captions' && items[i].track.label !== 'captions settings') {
+                items[i].el().style.display = 'none';
+            }
+        }
+    });
+
+    var tt = player.textTracks();
+    tt.addEventListener('change', function (e) {
+        for (var i = 0; i < tt.length; i++) {
+            if (tt[i].mode === 'showing') {
+                tt[i].mode = 'hidden';
+                this.removeEventListener('change', arguments.callee);
+            }
+        }
+    });
+});
